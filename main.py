3fd3e82557a07e100c4a80da5c636a6232574ea8
kuroneko <kskkm0228@gmail.com>
2019-03-23 11:05:38 +0900

    create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import django
+
+
+from django.db import models
+
+
+class Question(models.Model):
+    # ...
+    def __str__(self):
+        return self.question_text
+
+
+class Choice(models.Model):
+    def __str__(self): 
+        return self.choice_text
